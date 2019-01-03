0e510b485fd07395b0e6c1b3ad5b92f7b62f1ef4
joaohhenriq <joao_camargo10@hotmail.com>
2019-01-03 20:55:34 -0200

    Implementando o servidor

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Gulp - Projeto SPA
 
 Projeto SPA com foco na automatização do build do projeto com Gulp.
+
+Realizamos a automatização do build dos arquivos específicos para o projeto, dependências, e também a criação de uma classe de servidor, para monitorar as mudanças realizadas e restartar o servidor todas vez que for apresentado mudanças na implementação (fazendo uso do watch e webserver).
--- gulpTasks/servidor.js
+++ gulpTasks/servidor.js
@@ -1,9 +1,19 @@
 const gulp = require('gulp')
+const watch = require('gulp-watch')
+const webserver = require('gulp-webserver')
 
 gulp.task('monitorarMudancas', () => {
-
+    watch('src/**/*.html', () => gulp.start('app.html'))
+    watch('src/**/*.scss', () => gulp.start('app.css'))
+    watch('src/**/*.js', () => gulp.start('app.js'))
+    watch('src/assets/imgs/**/*.*', () => gulp.start('app.imgs'))
 })
 
 gulp.task('servidor', ['monitorarMudancas'], () => {
-    
+    return gulp.src('build')
+        .pipe(webserver({
+            livereload: true, //para dar reload na página
+            port: 8080,
+            open: true //abre o browser quando startar o servidor
+        }))
 })
