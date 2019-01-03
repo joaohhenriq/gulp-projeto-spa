286e3f187009ff4651a0fac47fb681e27b85a6c3
joaohhenriq <joao_camargo10@hotmail.com>
2019-01-03 20:23:30 -0200

    Implementando a automatização do build das dependências

--- gulpTasks/deps.js
+++ gulpTasks/deps.js
@@ -1,11 +1,18 @@
 const gulp = require('gulp')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
 
 gulp.task('deps', ['deps.css', 'deps.fonts'])
 
+// run-sequence apenas funciona se colocarmos return em cada um dos métodos
 gulp.task('deps.css', () => {
-
+    return gulp.src(['node_modules/font-awesome/css/font-awesome.css'])
+        .pipe(uglifycss({ "uglyComments": true }))
+        .pipe(concat('deps.min.css'))
+        .pipe(gulp.dest('build/assets/css'))
 })
 
 gulp.task('deps.fonts', () => {
-    
+    return gulp.src(['node_modules/font-awesome/fonts/*.*'])
+        .pipe(gulp.dest('build/assets/fonts'))
 })
